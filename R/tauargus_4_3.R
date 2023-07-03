150b9570aaf546e1aad22613b6e1bb68b04c22b3
Socard André-Raymond <ZOW2JK@ad.insee.intra>
2023-07-03 15:40:35 +0200

    tauargus

--- R/tauargus_4_3.R
+++ R/tauargus_4_3.R
@@ -17,7 +17,7 @@
 #' @return une liste avec de tableaux avec le secret posé
 #' 
 #'
-tau_argus_4_3<-function(list_res,liste_secret,totcode,freq,value=FALSE){
+tau_argus_4_3<-function(list_res,liste_secret,totcode,freq,value="FALSE",hrcfiles){
   
     
   
@@ -28,44 +28,30 @@ tau_argus_4_3<-function(list_res,liste_secret,totcode,freq,value=FALSE){
     
     #On récupère les varibales des différentes tables
 
-    var_cross<-paste(res$vars[1],res$vars[2],sep="_")
+    var_cross<-paste(list_res$vars[1],list_res$vars[2],sep="_")
     d<- intersect(names(list_res$tabs$T1), names(totcode))
 
-    v<-c(d,var_cross)
     
-    list_vars<-replicate(n,v,simplify=FALSE)
+    list_vars<-replicate(n,d,simplify=FALSE)
     names(list_vars)<- c(paste0("T",1:n,sep=""))
     
     # On pose le secret avec tauargus
     #Je vérifie si le tableaux est un tableaux de fréquence
     
-    if (value %in% names(list_res$tabs$T1)){
+   
     masq <- tab_multi_manager(
     list_tables = liste_secret,
     list_explanatory_vars = list_vars  ,
-    dir_name = "test_avec_rtauargus.R",
+    dir_name = "test_avec_rtauargus",
     totcode = totcode,
-    hrc = c(ACTIVITY=hrc_activity,NUTS23=hrc_nuts),
+    hrc = hrcfiles,
     alt_hrc = list_res$hrcs,
     alt_totcode = list_res$alt_tot,
     value = value,
     freq = freq,
     secret_var = "is_secret_prim")
     
-    return(masq) }
-    
-    
-    masq <- tab_multi_manager(
-        list_tables = liste_secret,
-        list_explanatory_vars = list_vars  ,
-        dir_name = "test_avec_rtauargus.R",
-        totcode = totcode,
-        hrc = c(ACTIVITY=hrc_activity,NUTS23=hrc_nuts),
-        alt_hrc = list_res$hrcs,
-        alt_totcode = list_res$alt_tot,
-        freq = freq,
-        secret_var = "is_secret_prim")
-        
+   
      
   return(masq)
 }
--- test/test_avec_rtauargus.R
+++ test/test_avec_rtauargus.R
@@ -7,17 +7,17 @@
 #####DONNEES########
 ####################
 
-rm(list = ls())
 
 library(stringr)
 source("R/function_passer_3_4.R")
 source("R/format.R")
-source("tauargus_4_3.R")
+source("R/tauargus_4_3.R")
 load("data/ca_pizzas_4vars.RData")
 source("R/cas_gen_4_3.R")
 library("purrr")
 library("dplyr")
 library("rtauargus")
+source("R/function_passer_3_4.R")
 loc_tauargus <- "C:/Users/ZOW2JK/Downloads/oui/TauArgus4.2.3/TauArgus.exe"
 options(rtauargus.tauargus_exe = loc_tauargus)
 
@@ -44,9 +44,9 @@ res<-passer_de_4_a_3_var(ca_pizzas_4vars,nom_dfs,totcode,hrcfiles,sep_dir = TRUE
 
 #On a le format utilisé pour rtauargus
 
-list_tabs<-tabs_5_4_to_3(ca_pizzas_4vars,nom_dfs,totcode,hrcfiles,sep_dir = TRUE,"output")
+list_res<-tabs_5_4_to_3(ca_pizzas_4vars,nom_dfs,totcode,hrcfiles,sep_dir = TRUE,"output")
 
-list_tab<-list_tabs$tabs
+list_tab<-list_res$tabs
 list_vars<-list(
   T1=c("ACTIVITY","NUTS23","treff_cj"),
   T2=names(list_tab$T2)[sapply(list_tab$T2, is.character)]
@@ -80,8 +80,8 @@ exemple_masq <- tab_multi_manager(
   dir_name = "test_avec_rtauargus",
   totcode = c(ACTIVITY="Total",NUTS23="Total",treff_cj="Total_Total"),
   hrc = c(ACTIVITY=hrc_activity,NUTS23=hrc_nuts),
-  alt_hrc = list_tabs$hrcs,
-  alt_totcode = list_tabs$alt_tot,
+  alt_hrc = list_res$hrcs,
+  alt_totcode = list_res$alt_tot,
   value = "pizzas_tot",
   maxscore = "pizzas_max",
   freq = "nb_obs",
@@ -91,8 +91,50 @@ exemple_masq <- tab_multi_manager(
 
 str(exemple_masq)
 
-tau<-tauargus_4_3(res,liste_tabs_exemple,totcode,"nb_obs","pizzas_tot")
+tot_code<-c(ACTIVITY="Total",NUTS23="Total",treff_cj="Total_Total")
+value<-"pizzas_tot"
+freq<-"nb_obs"
+
+tau<-tau_argus_4_3(list_res,liste_tabs_exemple,tot_code,freq,value,hrcfiles)
 identical(tau,exemple_masq)
+
+res_4_3<-list(
+  tabs=exemple_masq,
+  hrcs=res$hrcs,
+  vars=res$vars
+)
+
+p<-passer_3_41(res_4_3,ca_pizzas_4vars)
+
+p_compt <- p %>% 
+  mutate(
+    statut_final = case_when(
+      is_secret_freq ~ "A",
+      is_secret_dom ~ "B",
+      is_secret_4 ~ "D",
+      TRUE ~ "V"
+    )
+  )
+
+#nombre enlevé 
+p_compt %>% 
+  group_by(statut_final) %>% 
+  summarise(
+    n_cell = n(),
+    val_cell = sum(pizzas_tot)
+  ) %>%
+  mutate(
+    pc_n_cell = n_cell/sum(n_cell)*100,
+    pc_val_cell = val_cell/sum(val_cell)*100
+  )
+
+# A tibble: 4 × 5
+# statut_final n_cell   val_cell pc_n_cell pc_val_cell
+# <chr>         <int>      <dbl>     <dbl>       <dbl>
+#   1 A              2147  21701410.     19.8         3.13
+# 2 B               639  29207577.      5.89        4.22
+# 3 D              3890 141724101.     35.8        20.5 
+# 4 V              4178 499798732.     38.5        72.2
 
 # On essaye de comparer avec le tableau à 4 variables catégorielles
 
@@ -125,5 +167,66 @@ exemple_masq2 <- tab_rtauargus(
   verbose = FALSE
 )
 
+comptage_ex<-exemple_masq2%>% 
+  mutate(
+    statut_final = case_when(
+      is_secret_freq ~ "A",
+      is_secret_dom ~ "B",
+      TRUE ~ Status,
+    )
+  )
+
+comptage_ex %>% 
+  group_by(statut_final) %>% 
+  summarise(
+    n_cell = n(),
+    val_cell = sum(pizzas_tot)
+  ) 
+# On a 4222 lignes supprimés
+
+#######################################
+############DONNEES2###################
+#######################################
+
+
+data <- expand.grid(
+  ACT = c("Total",read.table("hrc/hrc2.hrc") %>% mutate(V1 = gsub("@?","",V1, perl = TRUE)) %>% pull(V1)),
+  SEX = c("Total",read.table("hrc/hrc3.hrc") %>% mutate(V1 = gsub("@?","",V1, perl = TRUE)) %>% pull(V1)),
+  GEO = c("Pays",read.table("hrc/hrc_REG_deep_3.hrc") %>% mutate(V1 = gsub("@?","",V1, perl = TRUE)) %>% pull(V1)),
+  AGE = c("Ensemble","adulte","enfant"),
+  stringsAsFactors = FALSE
+) %>% 
+  as.data.frame()
+
+data <- data %>% mutate(VALUE = runif(nrow(data)))
+hrc_files = c(ACT = "hrc/hrc2.hrc", GEO = "hrc/hrc_REG_deep_3.hrc", SEX = "hrc/hrc3.hrc" )
+
+tot_code<-c(SEX="Total",AGE="Ensemble", GEO="Pays", ACT="Total")
+
+
+# pour execution ligne à ligne
+dfs <- data
+nom_dfs <- "nom_data_frame"
+
+totcode <- tot_code
+hrcfiles <- hrc_files
+
+var_cat <- names(totcode)
+
+var_sans_hier <- intersect(
+  setdiff(names(dfs), names(hrcfiles)),
+  var_cat
+)
+
+v1 <- var_sans_hier[1]
+v2<-plus_petit_hrc(hrc_files,tot_code)
+
+
+
+dir_name <- dirname(hrcfiles[1])
+
+
+
+list_res2<-tabs_5_4_to_3(dfs,nom_dfs,totcode ,hrcfiles ,sep_dir=FALSE,hrc_dir="hrc_alt")
 
 
