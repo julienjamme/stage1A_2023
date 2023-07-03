1a35e8ede074f79271eef422c100d8aeca9703a4
Socard André-Raymond <ZOW2JK@ad.insee.intra>
2023-07-03 11:28:31 +0200

    test tau_argus

--- R/tauargus_4_3.R
+++ R/tauargus_4_3.R
@@ -1,36 +1,45 @@
 
 
-tau_argus_4_3<-function(list_res,totcode,freq,value){
-  
-  
-  
-  freqs<-as.character(freq)
-  values<-as.character(value)
-  list_tab<-list_res$tabs
-  n<-length(list_tab)
-  #Pour le moment après voir avec les modalités
-  res$vars
-  var_cross<-paste(res$vars[1],res$vars[2],sep="_")
 
-   d<- intersect(names(list_res$tabs$T1), names(totcode))
 
- 
-  v<-c(d,var_cross)
+
+
+#' tau_argus_4_3
+#'
+#' @param list_res liste de 3 éléments : 
+#'      - une liste de tableaux de 3 variables catégorielles
+#'      - une liste 
+#' @param liste_secret liste_res après avoir appliqué le secret primaire 
+#' @param totcode liste contenant les totaux 
+#' @param freq  nom de la colonne contenant les fréquences
+#' @param value nom de la colonne contenant les valeurs
+#'
+#' @return une liste avec de tableaux avec le secret posé
+#' 
+#'
+tau_argus_4_3<-function(list_res,liste_secret,totcode,freq,value=FALSE){
+  
     
-    # Secret primaire
+  
+    #Initilaisation
+  
+    list_tab<-list_res$tabs
+    n<-length(list_tab)
     
-    liste_secret <- lapply(list_tab, function(tab) {
-      tab$is_secret_freq <- tab$nb_obs > 0 & tab$nb_obs < 3
-      tab$is_secret_dom <- (tab$pizzas_tot != 0) & (tab$pizzas_max > 0.85 * tab$pizzas_tot)
-      tab$is_secret_prim <- tab$is_secret_freq | tab$is_secret_dom
+    #On récupère les varibales des différentes tables
 
-      return(tab)
-    })
+    var_cross<-paste(res$vars[1],res$vars[2],sep="_")
+    d<- intersect(names(list_res$tabs$T1), names(totcode))
+
+    v<-c(d,var_cross)
     
     list_vars<-replicate(n,v,simplify=FALSE)
-    
     names(list_vars)<- c(paste0("T",1:n,sep=""))
     
+    # On pose le secret avec tauargus
+    #Je vérifie si le tableaux est un tableaux de fréquence
+    
+    if (value %in% names(list_res$tabs$T1)){
     masq <- tab_multi_manager(
     list_tables = liste_secret,
     list_explanatory_vars = list_vars  ,
@@ -39,11 +48,25 @@ tau_argus_4_3<-function(list_res,totcode,freq,value){
     hrc = c(ACTIVITY=hrc_activity,NUTS23=hrc_nuts),
     alt_hrc = list_res$hrcs,
     alt_totcode = list_res$alt_tot,
-    value = values,
-    freq = freqs,
+    value = value,
+    freq = freq,
     secret_var = "is_secret_prim",
     
-  )
-  
+    return(masq)
+    )}
+    
+    
+    masq <- tab_multi_manager(
+        list_tables = liste_secret,
+        list_explanatory_vars = list_vars  ,
+        dir_name = "test_avec_rtauargus.R",
+        totcode = totcode,
+        hrc = c(ACTIVITY=hrc_activity,NUTS23=hrc_nuts),
+        alt_hrc = list_res$hrcs,
+        alt_totcode = list_res$alt_tot,
+        freq = freq,
+        secret_var = "is_secret_prim",
+        
+     
   return(masq)
 }
--- test/test_avec_rtauargus.R
+++ test/test_avec_rtauargus.R
@@ -1,15 +1,21 @@
 
 
 # Vider l'environnement global
-# Vider l'environnement global
+# Ne pas faire de controle A + Enter 
+
+####################
+#####DONNEES########
+####################
+
 rm(list = ls())
 
 library(stringr)
 source("R/function_passer_3_4.R")
 source("R/format.R")
+source("tauargus_4_3.R")
 load("data/ca_pizzas_4vars.RData")
 source("R/cas_gen_4_3.R")
-
+library("purrr")
 library("dplyr")
 library("rtauargus")
 loc_tauargus <- "C:/Users/ZOW2JK/Downloads/oui/TauArgus4.2.3/TauArgus.exe"
@@ -39,44 +45,18 @@ res<-passer_de_4_a_3_var(ca_pizzas_4vars,nom_dfs,totcode,hrcfiles,sep_dir = TRUE
 #On a le format utilisé pour rtauargus
 
 list_tabs<-tabs_5_4_to_3(ca_pizzas_4vars,nom_dfs,totcode,hrcfiles,sep_dir = TRUE,"output")
-str(list_tabs)
-str(list_tabs$tabs$T1)
-
-tabs_detect <-list_tabs$tabs$T1 %>%
-  mutate(
-    is_secret_freq = nb_obs > 0 & nb_obs < 3,
-    is_secret_dom = (pizzas_tot != 0) & (pizzas_max > 0.85*pizzas_tot)
-  ) %>% 
-  mutate(
-    is_secret_prim = is_secret_freq | is_secret_dom,
-    
-  )
-
-tabs_detect %>% count(is_secret_prim)
-
-str(ca_pizzas_4vars)
-str(tabs_detect)
-T0_masq <- tab_rtauargus(
-  tabs_detect,
-  dir_name = "test/test_avec_rtauargus.R",
-  files_name = "test",
-  explanatory_vars = c("ACTIVITY","NUTS23","treff_cj"),
-  secret_var = "is_secret_prim",
-  hrc = list(ACTIVITY=hrc_activity,NUTS23=hrc_nuts,treff_cj=list_tabs$hrcs$T1$treff_cj),
-  #alt_hrc=format 
-  value = "pizzas_tot",
-  freq = "nb_obs",
-  totcode = c(ACTIVITY="Total",NUTS23="Total",treff_cj="Total_Total"),
-  verbose = FALSE
-)
-
-str(T0_masq)
 
 list_tab<-list_tabs$tabs
 list_vars<-list(
   T1=c("ACTIVITY","NUTS23","treff_cj"),
   T2=names(list_tab$T2)[sapply(list_tab$T2, is.character)]
 )
+
+####################
+######TEST##########
+####################
+
+# On pose le secret primaire
 
 liste_tabs_exemple <- purrr::map(
   list_tab,
@@ -91,19 +71,19 @@ liste_tabs_exemple <- purrr::map(
         nb_obs = ceiling(nb_obs)
 )})
 
-
-str(liste_tabs_exemple)
+# On pose le secret secondaire 
 
 exemple_masq <- tab_multi_manager(
   list_tables = liste_tabs_exemple,
   list_explanatory_vars = list(T1=c("ACTIVITY","NUTS23","treff_cj"),
                                T2=c("ACTIVITY","NUTS23","treff_cj")) ,
-  dir_name = "test/test_avec_rtauargus.R",
+  dir_name = "test_avec_rtauargus",
   totcode = c(ACTIVITY="Total",NUTS23="Total",treff_cj="Total_Total"),
   hrc = c(ACTIVITY=hrc_activity,NUTS23=hrc_nuts),
   alt_hrc = list_tabs$hrcs,
   alt_totcode = list_tabs$alt_tot,
   value = "pizzas_tot",
+  maxscore = "pizzas_max",
   freq = "nb_obs",
   secret_var = "is_secret_prim",
   
@@ -111,13 +91,39 @@ exemple_masq <- tab_multi_manager(
 
 str(exemple_masq)
 
-list_masques <- as.data.frame(lapply(exemple_masq[[1]], function(col) {
-  if (any(col == FALSE)) {
-    exemple_masq[[1]]$pizzas_tot <- NA
-    exemple_masq[[1]]$pizzas_max <- NA
-    exemple_masq[[1]]$nb_obs <- NA
-  }
-  return(col)
-}))
+tau<-tauargus_4_3(res,liste_tabs_exemple,totcode,"nb_obs","pizzas_tot")
+identical(tau,exemple_masq)
+
+# On essaye de comparer avec le tableau à 4 variables catégorielles
+
+tabs_exemple <- (
+  ca_pizzas_4vars %>% 
+
+      mutate(
+        is_secret_freq = nb_obs > 0 & nb_obs < 3,
+        is_secret_dom = (pizzas_tot != 0) & (pizzas_max > 0.85*pizzas_tot),
+        pizzas_tot= abs(pizzas_tot)
+      ) %>% 
+      mutate(
+        is_secret_prim = is_secret_freq | is_secret_dom,
+        nb_obs = ceiling(nb_obs)
+      ))
+
+# Ne pas tourner car long 
+# Exemple de tableau à 4 variables 
+
+exemple_masq2 <- tab_rtauargus(
+  tabs_exemple,
+  files_name = "ca_pizzas_4vars" ,
+  explanatory_vars = c("ACTIVITY","NUTS23","treff","cj"),
+  dir_name = "test_avec_rtauargus",
+  totcode = c(ACTIVITY="Total",NUTS23="Total",treff="Total",cj="Total"),
+  hrc = c(ACTIVITY=hrc_activity,NUTS23=hrc_nuts),
+  value = "pizzas_tot",
+  freq = "nb_obs",
+  secret_var = "is_secret_prim",
+  verbose = FALSE
+)
+
 
 
