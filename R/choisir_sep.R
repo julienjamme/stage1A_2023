8d68094b5a58d7c24117915cea3a188b399ac90e
julienjamme <julien.jamme@insee.fr>
2023-07-21 10:29:54 +0200

    revue sep par defaut

--- R/choisir_sep.R
+++ R/choisir_sep.R
@@ -10,6 +10,7 @@
 #' @export
 #' 
 #' @examples
+#' library(dplyr)
 #' data <- expand.grid(
 #'   AGE = c("+","ça va ?","Mais non, Pas possible !!!"),
 #'   ECO = c("ça fait 5€"),
@@ -17,19 +18,18 @@
 #' ) %>%
 #'   as.data.frame()
 #' 
-#' 
 #' liste_sep = c("\\+", "\\!", "\\?","\\$","\\£",
 #'               "\\€","\\:","\\;","\\~","\\&",
 #'               "\\#")
-#' choisir_sep(data,liste_sep = liste_sep) == "$$$"
-#' TRUE
+#' choisir_sep(data,liste_sep = liste_sep) == "$"
 #' 
 #' 
 #' liste_sep = c("\\+", "\\!", "\\?")
-#' choisir_sep(data,liste_sep = liste_sep) == "+!?"
-#' TRUE
-choisir_sep <- function(data, liste_sep = c("\\_+", "\\_!", "\\_?","\\_:",
-                                            "\\_;","\\_~","\\_&","\\_#")){
+#' choisir_sep(data,liste_sep = liste_sep)
+#' 
+#' choisir_sep(data)
+choisir_sep <- function(data, liste_sep = c("\\_+_", "\\_!_", "\\_?_")){
+  require(stringr)
   
   liste_var<-names(data)
   liste_mod<-unique(unlist(lapply(data, unique)))
@@ -41,24 +41,25 @@ choisir_sep <- function(data, liste_sep = c("\\_+", "\\_!", "\\_?","\\_:",
   while (i <= n_sep & is_in_mod){
     i <- i + 1
     sep <- liste_sep[i]
-    is_in_mod = sum(unlist(lapply(liste_mod, function(x) str_detect(x,sep)))) > 0
+    is_in_mod = sum(unlist(lapply(liste_mod, function(x) stringr::str_detect(x,sep)))) > 0
   }
   
   # On a un sépérateur qui fonctionne !
   if (i  <= n_sep){
     # On enlève les \ devant
-    sep <- str_sub(liste_sep[i], start = 2)
+    sep <- stringr::str_sub(liste_sep[i], start = 2)
     
     # On renvoie le triple du séparateur
     return(paste0(sep, collapse = ""))
   } else {
-    # Tous les séparateurs sont présents dans les modalités
-    # On concatène tous les séparateurs pour construire un super séparateur
-    return(str_flatten(
-      lapply(liste_sep,
-             function(x) str_sub(x, start = 2)
-      )
-    )
-    )
+    return(paste(rep("_+",4), collapse = ""))
+    # # Tous les séparateurs sont présents dans les modalités
+    # # On concatène tous les séparateurs pour construire un super séparateur
+    # return(stringr::str_flatten(
+    #   lapply(liste_sep,
+    #          function(x) stringr::str_sub(x, start = 2)
+    #   )
+    # )
+    # )
   }
 }
