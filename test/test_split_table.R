a073098f07c742c08b27636b46bc2edc0a4c716f
Pomel Wistan <PWE6YK@ad.insee.intra>
2023-07-28 10:16:09 +0200

    Update of the data of the test

--- test/test_split_table.R
+++ test/test_split_table.R
@@ -11,25 +11,44 @@ source("R/chercher_combinaison_variable_a_fusionner.R",encoding = "UTF-8")
 source("R/split_table.R",encoding = "UTF-8")
 
 data <- expand.grid(
-  ACT = c("Total",read.table("hrc/hrc1.hrc") %>% mutate(V1 = gsub("@?","",V1, perl = TRUE)) %>% pull(V1)),
-  GEO = c("Total",read.table("hrc/hrc2.hrc") %>% mutate(V1 = gsub("@?","",V1, perl = TRUE)) %>% pull(V1)),
-  SEX = c("Total",LETTERS[1:5]),
-  AGE = c("Total",LETTERS[21:25]),
-  stringsAsFactors = FALSE
+  ACT = c("Total", "A", "B", "A1", "A2", "B1", "B2"),
+  GEO = c("Total", "GA", "GB", "GA1", "GA2", "GB1", "GB2"),
+  SEX = c("Total", "F", "M"),
+  AGE = c("Total", "AGE1", "AGE2"),
+  stringsAsFactors = FALSE,
+  KEEP.OUT.ATTRS = FALSE
 ) %>% 
   as.data.frame()
 
-data <- data %>% mutate(VALUE = runif(nrow(data)))
-#hrcfiles = c(ACT = "hrc/hrc1.hrc", GEO = "hrc/hrc2.hrc")
-hrcfiles = c(GEO = "hrc/hrc2.hrc")
-totcode<-c(SEX="Total",AGE="Total", GEO="Total", ACT="Total")
+data <- data %>% mutate(VALUE = 1:n())
+nrow(data) #441 rows = 7*7*3^2
 
+hrc_act <- "output/hrc_ACT.hrc"
+sdcHierarchies::hier_create(root = "Total", nodes = c("A","B")) %>% 
+  sdcHierarchies::hier_add(root = "A", nodes = c("A1","A2")) %>% 
+  sdcHierarchies::hier_add(root = "B", nodes = c("B1","B2")) %>% 
+  sdcHierarchies::hier_convert(as = "argus") %>%
+  slice(-1) %>% 
+  mutate(levels = substring(paste0(level,name),3)) %>% 
+  select(levels) %>% 
+  write.table(file = hrc_act, row.names = F, col.names = F, quote = F)
 
-# pour execution ligne à ligne
+hrc_geo <- "output/hrc_GEO.hrc"
+sdcHierarchies::hier_create(root = "Total", nodes = c("GA","GB")) %>% 
+  sdcHierarchies::hier_add(root = "GA", nodes = c("GA1","GA2")) %>% 
+  sdcHierarchies::hier_add(root = "GB", nodes = c("GB1","GB2")) %>% 
+  sdcHierarchies::hier_convert(as = "argus") %>%
+  slice(-1) %>% 
+  mutate(levels = substring(paste0(level,name),3)) %>% 
+  select(levels) %>% 
+  write.table(file = hrc_geo, row.names = F, col.names = F, quote = F)
+
+totcode <- c(SEX="Total",AGE="Total", GEO="Total", ACT="Total")
+nom_dfs <- "nom"
+hrcfiles <- c(ACT = hrc_act, GEO = hrc_geo)
+hrc_dir <- "output"
 dfs <- data
-nom_dfs <- "nom_data_frame"
-sep_dir = TRUE
-hrc_dir = "test/test_fonction_general/test2"
+sep_dir <- TRUE
 
 res1 <-gen_tabs_5_4_to_3(dfs,nom_dfs,totcode ,hrcfiles,
                            sep_dir = sep_dir,
@@ -49,6 +68,6 @@ res_f <- split_tab(res = res1,
                    var_fus = paste(res1$fus_vars[1],
                                     res1$fus_vars[2],
                                     sep = res1$sep),
-                   LIMIT = 1500)
+                   LIMIT = 150)
 
 max(sapply(res_f$tabs, nrow))
